ec14a374e6795e9c5dc81fefd9a9dc18e99ecf8d
obi chinedu frank <frankieetchy@gmail.com>
2019-02-10 11:58:33 +0100

    installed eslint

--- back/index.js
+++ back/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/home', (req, res)=>{
-    res.send({
-        message:'home'
-    })
+app.get('/home', (req, res) => {
+  res.send({
+    message: 'home',
+  });
 });
 
 const PORT = 5000;
 
-app.listen(PORT, ()=>{
-    console.log('server start at port 5000');
-})
+app.listen(PORT, () => {
+  console.log('server start at port 5000');
+});
