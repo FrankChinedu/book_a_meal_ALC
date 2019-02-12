efca64f35dffdeac69043c7b7140473881affe89
obi chinedu frank <frankieetchy@gmail.com>
2019-02-12 16:22:43 +0100

    required cors and body-parser

--- back/index.js
+++ back/index.js
@@ -1,8 +1,13 @@
 import 'dotenv/config';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const routes = require('./routes/api');
 
