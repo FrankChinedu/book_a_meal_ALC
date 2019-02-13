89d93c46acb216900ed0a3d2fa06b2fd69b958c8
obi chinedu frank <frankieetchy@gmail.com>
2019-02-13 07:07:15 +0100

    install morgan

--- back/index.js
+++ back/index.js
@@ -3,10 +3,12 @@ import 'dotenv/config';
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
